419e30030a57f36e22d89d9caf5f2bd9b7d52935
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-10-14 02:10:24 -0400

    First commit: the code is compiling

---
+++ src/LoadRunnerGitAddIn/GitMessageView.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.Linq;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace MyLoadTest.LoadRunnerGitAddIn
+{
+	/// <summary>
+	/// Output pad category for git.
+	/// </summary>
+	public static class GitMessageView
+	{
+		static MessageViewCategory category;
+		
+		/// <summary>
+		/// Gets the git message view category.
+		/// </summary>
+		public static MessageViewCategory Category {
+			get {
+				if (category == null) {
+					MessageViewCategory.Create(ref category, "Git");
+				}
+				return category;
+			}
+		}
+		
+		/// <summary>
+		/// Appends a line to the git message view.
+		/// </summary>
+		public static void AppendLine(string text)
+		{
+			Category.AppendLine(text);
+		}
+	}
+}
